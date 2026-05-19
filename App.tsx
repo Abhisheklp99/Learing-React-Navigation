980dd4e0f85c3cbf2227e10552d16d80da1d0837
Abhishek16oS <Abhishek16business@protonmail.com>
2026-05-19 13:22:49 +0530

    feat:top tab added

--- App.tsx
+++ App.tsx
@@ -18,7 +18,7 @@ export default function App(){
 //For Tabs that is Statics
 
 
-
+/* 
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import {
@@ -69,3 +69,63 @@ export default function App() {
 
 
 
+ */
+
+
+// ***************************************
+// ***************************************
+// ***************************************
+//Top tab
+
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { useNavigation, NavigationContainer } from '@react-navigation/native';
+import { Button } from '@react-navigation/elements';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+function HomeScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Profile')}>
+        Go to Profile
+      </Button>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView>
+
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+      <Button onPress={() => navigation.navigate('Home')}>Go to Home</Button>
+    </View>
+    </SafeAreaView>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
